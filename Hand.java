d3ba16bec713b12e068629e347b3800ef3a9d225
Perhashy <dr.hashy-hcr1002@ezweb.ne.jp>
2020-08-16 20:38:07 +0900

    メッセージ追加

--- Hand.java
+++ Hand.java
@@ -12,14 +12,17 @@ public class Hand {
   public void setPlayerHand() {
     Scanner sc = new Scanner(System.in);
     try {
+      System.out.println("手を数字で入力してください");
+      System.out.print("(1：グー、2：チョキ、3：パー)：");
       int playerHand = sc.nextInt();
       if (playerHand > 0 && playerHand <= 3) {
         this.hand = playerHand;
       } else {
+        System.out.println("正しくない手を出しました、あなたの負けです");
         System.exit(0);
       }
     } catch (InputMismatchException e) {
-      System.out.println("数値以外が入力されました");
+      System.out.println("数値以外が入力されました、ゲームを終了します");
       System.exit(0);
     }
   }
